a47bc37843acffdd98ae56ef9c16198cf7736dae
Victoria <victoria.andujar10@gmail.com>
2020-09-30 17:32:37 -0300

    Initial config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import GlobalStyle from "./global/styles";
+
+const App: React.FC = () => (
+  <>
+    <h1>Olá</h1>
+    <GlobalStyle />
+  </>
+);
+
+export default App;
