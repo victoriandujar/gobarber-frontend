3b16d5bab2cfe7972b9dd509aba563abd3827b25
Victoria <victoria.andujar10@gmail.com>
2020-10-02 16:47:59 -0300

    Add Animate Toast

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
+import { useTransition } from "react-spring";
 import { ToastMessage } from "../../context/ToastContext";
 
 import ToastTwo from "./ToastTwo";
-
 import { Container } from "./styles";
 
 interface ToastContainerProps {
@@ -10,10 +10,20 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: "-120%", opacity: 0 },
+      enter: { right: "0%", opacity: 1 },
+      leave: { right: "-120%", opacity: 0 },
+    }
+  );
+
   return (
     <Container>
-      {messages.map((message) => (
-        <ToastTwo key={message.id} message={message} />
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <ToastTwo key={key} message={item} style={props} />
       ))}
     </Container>
   );
